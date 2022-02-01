928f3a01d355d3517167900d15539658fa3dadee
solsjo <oskar.solsjo@gmail.com>
2022-02-01 23:49:57 +0100

    Update ghdl.sh

--- src/ghdl.sh
+++ src/ghdl.sh
@@ -2,6 +2,22 @@
 
 set -e
 
-# ghdl invocationn
+ghdl_args="$@"
+
+if [ -n "$DOCKER_IMAGE" ]; then
+# regex explaination, we want to extract the output base
+# the output base usually ends with
+# _bazel_<user name>/<MD5 of workspace directory>
+output_base_path=$(readlink -f $(pwd) | \
+                   grep -Eow .*/_bazel_[^/]*/[0-9a-zA-Z]*)
+
+docker run --rm -t \
+  --user "$(id -u):$(id -g)" \
+  --volume $output_base_path:$output_base_path \
+  --workdir "$PWD" \
+  "$DOCKER_IMAGE" sh -c "$ghdl_args"
+else
+$ghdl_args
+fi
 
 exit $?
