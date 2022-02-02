35b9a41a856596fe5c73c0be8774e8330567178e
solsjo <oskar.solsjo@gmail.com>
2022-02-02 22:47:46 +0100

    Update ghdl.sh

--- src/ghdl.sh
+++ src/ghdl.sh
@@ -22,25 +22,11 @@ docker run --rm -t \
   "$DOCKER_IMAGE" sh -c "$ghdl_args"
 else
 
-# regex explaination, we want to extract the output base
-# the output base usually ends with
-# _bazel_<user name>/<MD5 of workspace directory>
-output_base_path=$(readlink -f $(pwd) | \
-                   grep -Eow .*/_bazel_[^/]*/[0-9a-zA-Z]*)
-
-
-docker run --rm -t \
-  --user "$(id -u):$(id -g)" \
-  --volume $output_base_path:$output_base_path \
-  --workdir "$PWD" \
-  ghdl/vunit:llvm-master sh -c "$ghdl_args"
-
-
 echo "first $ghdl_args"
 
 echo "hello $cmd_arr"
 #echo "hello2 ${cmd_arr[@]}"
-#"${cmd_arr}"
+"${cmd_arr}"
 fi
 
 exit $?
