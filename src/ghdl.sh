6db916a038e23af6a1cf7b39d8209b724318a352
solsjo <oskar.solsjo@gmail.com>
2022-02-05 07:54:32 +0100

    Update ghdl.sh

--- src/ghdl.sh
+++ src/ghdl.sh
@@ -42,11 +42,12 @@ echo "hello4 ${cmd_arr}"
 echo "hello2 ${cmd_arr[@]}"
 echo "pwd: $PWD"
 ls -la
-ls -la bazel-out/k8-fastbuild/bin/external/ghdl_toolchain/bin
+ls -la external
+ls -la external/ghdl_toolchain/bin
 #ls -la ./external/ghdl_toolchain/bin
 #${cmd_arr}
 echo ">>>>>>>>>>>"
-bazel-out/k8-fastbuild/bin/external/ghdl_toolchain/bin/ghdl --version
+external/ghdl_toolchain/bin/ghdl --version
 echo "++++++++"
 "$ghdl_args"
 
