bcd1da3a93f3aa730e137e0cac52c0b6b1c5c2cb
solsjo <oskar.solsjo@gmail.com>
2022-02-06 18:52:23 +0100

    Update ghdl.sh

--- src/ghdl.sh
+++ src/ghdl.sh
@@ -7,13 +7,8 @@ new_lib_file=${2}
 work_dir=${3}
 ghdl_bin=${4}
 shift 4
-echo "first>>>>>>>>>>>>>>>>>>>>>"
-#ghdl_args="$@"
 
 readarray -td, ghdl_args <<<"$@"
-echo "cmds::: ${ghdl_args[@]}"
-#./external/ghdl_toolchain/bin/ghdl --version
-echo "****"
 
 if [ -n "$old_lib_file" ]; then
   echo "copying $old_lib_file"
@@ -24,21 +19,20 @@ fi
 cd "$work_dir"
 
 if [ -n "$DOCKER_IMAGE" ]; then
-# regex explaination, we want to extract the output base
-# the output base usually ends with
-# _bazel_<user name>/<MD5 of workspace directory>
-output_base_path=$(readlink -f $(pwd) | \
-                   grep -Eow .*/_bazel_[^/]*/[0-9a-zA-Z]*)
+  # regex explaination, we want to extract the output base
+  # the output base usually ends with
+  # _bazel_<user name>/<MD5 of workspace directory>
+  output_base_path=$(readlink -f $(pwd) | \
+    grep -Eow .*/_bazel_[^/]*/[0-9a-zA-Z]*)
 
-docker run --rm -t \
-  --user "$(id -u):$(id -g)" \
-  --volume $output_base_path:$output_base_path \
-  --workdir "$PWD" \
-  "$DOCKER_IMAGE" sh -c "$ghdl_args"
+  docker run --rm -t \
+    --user "$(id -u):$(id -g)" \
+    --volume $output_base_path:$output_base_path \
+    --workdir "$PWD" \
+    "$DOCKER_IMAGE" sh -c "$ghdl_bin $ghdl_args"
 else
-ls "$ghdl_bin"
-echo "::::::::::"
-"$ghdl_bin" ${ghdl_args[@]}
+
+  "$ghdl_bin" ${ghdl_args[@]}
 
 fi
 
